a98d40cc293ebd344a11bf1ffb209a8341d9e211
Seth Bromberger <github@bromberger.com>
2022-12-30 13:17:04 -0500

    fixed testip.sh

--- testip.sh
+++ testip.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-BLACKLIST=$(grep "^BLACKLIST" geoip.sh | cut -d "=" -f 2 | tr -d '"')
-NGROUPS=$(tail -1 $BLACKLIST | cut -d " " -f 2 | sed "s/geoip//")
-NETSET=$(grep "^NETSET" geoip.sh | cut -d "=" -f 2 | tr -d '"')
+BLOCKLIST=$(grep "^BLOCKLIST=" runblocklist.sh | cut -d "=" -f 2 | tr -d '"')
+BLOCKTMPSET=$(grep "^BLOCKTMPSET=" runblocklist.sh | cut -d "=" -f 2 | tr -d '"')
+NGROUPS=$(tail -1 $BLOCKLIST | cut -d " " -f 2 | sed "s/$BLOCKTMPSET//")
+NETSET=$(grep "^NETSET" runblocklist.sh | cut -d "=" -f 2 | tr -d '"')
 for i in $(seq $NGROUPS); do 
   sudo ipset test $NETSET$i $1
 done
